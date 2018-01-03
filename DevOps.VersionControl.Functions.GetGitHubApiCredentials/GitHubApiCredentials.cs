09649a0e56d8f8cd00676ceec34de0a7577697b0
Christopher Dorst <chris@dorst.email>
2018-01-03 04:31:22 -0600

    Add GitHubApiCredentials

---
+++ DevOps.VersionControl.Functions.GetGitHubApiCredentials/GitHubApiCredentials.cs
@@ -0,0 +1,18 @@
+﻿using Common.Structures.HttpBasicAuthentication;
+using static System.Environment;
+
+namespace DevOps.VersionControl.Functions.GetGitHubApiCredentials
+{
+    public static class GitHubApiCredentials
+    {
+        private const string EnvironmentVariableName = "GITHUB_API_PERSONAL_ACCESS_TOKEN";
+
+        public static BasicAuthenticationCredentials GetCredentials(string user, string personalAccessToken = null)
+            => new BasicAuthenticationCredentials(user, GetSecret(personalAccessToken));
+
+        private static string GetSecret(string personalAccessToken)
+            => string.IsNullOrWhiteSpace(personalAccessToken)
+                ? GetEnvironmentVariable(EnvironmentVariableName)
+                : personalAccessToken;
+    }
+}
